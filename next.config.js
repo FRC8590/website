a95f9057b9be2d383deda8fc6abde4142e97fc5b
Peter Bierma <zintensitydev@gmail.com>
2024-09-20 17:38:01 -0400

    Clean.

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ["placehold.co", "cdn.worldvectorlogo.com"],
-        dangerouslyAllowSVG: true,
-    },
-};
-
-module.exports = nextConfig;
--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Image from "next/image";
-export default function Home() {
-    return (
-        <>
-            <h1>Woodson Robotics</h1>
-        </>
-    );
-}
