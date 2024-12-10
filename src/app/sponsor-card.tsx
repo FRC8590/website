ed0f55c79269840e2dea7e38c13e0706e26cd3c2
Peter Bierma <zintensitydev@gmail.com>
2024-12-09 20:12:47 -0500

    Fix hovering outside of the donation box causing the price to expand. (#8)
    #7 introduced a regression in which hovering next to the donation price
    text would cause it to expand. This fixes that by forcing the width with
    `w-fit`.

--- src/app/sponsor-card.tsx
+++ src/app/sponsor-card.tsx
@@ -51,7 +51,7 @@ function SponsorTierTitle(props: { name: string; amount: string }) {
             <header className="text-xl font-light select-none">
                 {props.name}
             </header>
-            <div className="text-5xl font-bold select-none hover:scale-105 transition-all">
+            <div className="text-5xl font-bold select-none hover:scale-105 transition-all w-fit">
                 <span className="font-normal text-xl text-zinc-200 select-none">
                     $
                 </span>
