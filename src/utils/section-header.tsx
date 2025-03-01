630879df6b98602b2d2d363edea02149c6d0ff6f
Peter Bierma <zintensitydev@gmail.com>
2025-03-01 10:31:09 -0500

    Add a gradient to the section header text.

--- src/utils/section-header.tsx
+++ src/utils/section-header.tsx
@@ -5,16 +5,18 @@ import { motion } from "framer-motion";
 export default function SectionHeader(props: HasChildren) {
     return (
         <motion.header
-            initial={{ opacity: 0.0 }}
-            whileInView={{ opacity: 1 }}
+            initial={{ opacity: 0.0, y: 60 }}
+            whileInView={{ opacity: 1, y: 0 }}
             transition={{
                 delay: 0.2,
                 duration: 0.6,
                 ease: "easeInOut",
             }}
-            className="text-center py-4 xl:py-8 text-3xl lg:text-4xl xl:text-6xl font-semibold text-black dark:text-white"
+            className="flex items-center justify-center w-full py-4 xl:py-8 text-3xl lg:text-4xl xl:text-6xl font-semibold text-black dark:text-white"
         >
-            {props.children}
+            <div className="h-fit w-fit bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-zinc-600 via-zinc-100 to-zinc-700 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+                <span className="">{props.children}</span>
+            </div>
         </motion.header>
     );
 }
