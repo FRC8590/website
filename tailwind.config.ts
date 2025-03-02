122a01b280de447e0d2e72d1673e30cc58ece7b7
Peter Bierma <zintensitydev@gmail.com>
2025-03-02 08:28:12 -0500

    Fix Tailwind search path.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -23,6 +23,7 @@ const config: Config = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
     ],
     theme: {
         extend: {
