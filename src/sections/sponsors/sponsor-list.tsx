216372e38c2cfa035147b86d6f05d35c0f98d6d4
Peter Bierma <zintensitydev@gmail.com>
2025-03-02 08:54:12 -0500

    Add an infinite carousel for the sponsors.

--- src/sections/sponsors/sponsor-list.tsx
+++ src/sections/sponsors/sponsor-list.tsx
@@ -1,6 +1,53 @@
 "use client";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import { InfiniteMovingCards } from "@/components/infinite-moving-cards";
+
+const SPONSORS = [
+    <Image
+        src="/sponsors/wt-ptso.png"
+        width={100}
+        height={100}
+        alt="C.G. Woodson PTSO"
+        className="sponsor-image"
+    />,
+    <Image
+        src="/sponsors/colonial-pipeline-co.svg"
+        width={200}
+        height={200}
+        alt="Colonial Pipeline Co."
+        className="sponsor-image"
+    />,
+    <Image
+        src="/sponsors/haas.png"
+        width={100}
+        height={100}
+        alt="HAAS"
+        className="sponsor-image"
+    />,
+    <Image
+        src="/sponsors/synergy.png"
+        width={200}
+        height={200}
+        alt="Synergy"
+        className="sponsor-image"
+    />,
+    <Image
+        src="/sponsors/mamma_lucia.png"
+        width={100}
+        height={100}
+        alt="Mamma Lucia"
+        className="sponsor-image"
+    />,
+    <Image
+        src="/sponsors/jctm.png"
+        width={200}
+        height={200}
+        alt="JCTM"
+        className="sponsor-image"
+    />,
+];
+
 export default function SponsorList() {
     return (
         <motion.div
@@ -12,51 +59,7 @@ export default function SponsorList() {
                 ease: "easeInOut",
             }}
         >
-            <p className="">As Trusted By</p>
-            <div className="grid gap-3 grid-cols-4 grid-rows-3 place-content-center place-items-center auto-rows-fr">
-                <Image
-                    src="/sponsors/wt-ptso.png"
-                    width={100}
-                    height={100}
-                    alt="C.G. Woodson PTSO"
-                    className="sponsor-image"
-                />
-                <Image
-                    src="/sponsors/colonial-pipeline-co.svg"
-                    width={200}
-                    height={200}
-                    alt="Colonial Pipeline Co."
-                    className="sponsor-image"
-                />
-                <Image
-                    src="/sponsors/haas.png"
-                    width={100}
-                    height={100}
-                    alt="HAAS"
-                    className="sponsor-image"
-                />
-                <Image
-                    src="/sponsors/synergy.png"
-                    width={200}
-                    height={200}
-                    alt="Synergy"
-                    className="sponsor-image"
-                />
-                <Image
-                    src="/sponsors/mamma_lucia.png"
-                    width={100}
-                    height={100}
-                    alt="Mamma Lucia"
-                    className="sponsor-image"
-                />
-                <Image
-                    src="/sponsors/jctm.png"
-                    width={200}
-                    height={200}
-                    alt="JCTM"
-                    className="sponsor-image"
-                />
-            </div>
+            <InfiniteMovingCards items={SPONSORS} pauseOnHover={false} />
         </motion.div>
     );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,6 +35,7 @@ const config: Config = {
                 spotlight: "spotlight 4s ease .75s 1 forwards",
                 aurora: "aurora 60s linear infinite",
                 "meteor-effect": "meteor 5s linear infinite",
+                scroll: "scroll var(--animation-duration, 40s) var(--animation-direction, forwards) linear infinite",
             },
             keyframes: {
                 spotlight: {
@@ -64,6 +65,11 @@ const config: Config = {
                     "100%": {
                         transform: "rotate(215deg) translateX(-500px)",
                         opacity: "0",
+                    },
+                },
+                scroll: {
+                    to: {
+                        transform: "translate(calc(-50% - 0.5rem))",
                     },
                 },
             },
