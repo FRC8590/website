2a48628826e18c39f35bee0ac63d471710fc4cc9
Peter Bierma <zintensitydev@gmail.com>
2025-04-13 10:56:09 -0400

    Make article text smaller on mobile.

--- src/utils/article.tsx
+++ src/utils/article.tsx
@@ -33,7 +33,7 @@ export default function Article(
                                 {props.title}
                             </span>
                         </div>
-                        <div className="flex flex-col space-y-6 text-base lg:text-lg text-zinc-200">
+                        <div className="flex flex-col space-y-6 text-sm md:text-base lg:text-lg text-zinc-200">
                             <p>{props.children}</p>
                             {props.extra && props.extra}
                         </div>
