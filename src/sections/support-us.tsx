b51ed4f232074587398b758a34efff4a886a270d
Peter Bierma <zintensitydev@gmail.com>
2025-11-14 18:57:49 -0500

    Use a cooler donation system.

--- src/sections/support-us.tsx
+++ src/sections/support-us.tsx
@@ -1,6 +1,7 @@
 import Article from "@/utils/article";
 import Br from "@/utils/br";
 import Image from "next/image";
+import { PinContainer } from "@/components/3d-pin";
 
 const DONATE_URL =
     "https://educatefairfax.app.neoncrm.com/forms/woodson-robotics";
@@ -20,24 +21,15 @@ export function DonateButton() {
 
 export default function SupportUs() {
     return (
-        <Article
-            image={
-                <Image
-                    src="/pictures/why-donate.jpg"
-                    width={500}
-                    height={500}
-                    alt="Why Donate?"
-                    className="grayscale rounded-tr-lg rounded-br-lg hover:grayscale-0 transition-all w-full"
-                />
-            }
-            title="Why Donate?"
-            extra={<DonateButton />}
-        >
-            Consider donating to help provide STEM opportunities to all students
-            at Woodson. By donating, you help foster student&apos;s passions by
-            enabling our team to purchase the necessary materials, tools, and
-            resources for building our robot. <Br />
-            Join us by empowering the next generation of innovators and leaders.
-        </Article>
+        <PinContainer title="Donate Now!" href="https://google.com">
+            <div className="p-4 text-lg flex flex-col tracking-tight h-full dark:bg-grid-small-white/[0.05] bg-grid-small-black/[0.2] w-full rounded-xl border border-[rgba(255,255,255,0.10)] dark:bg-zinc-950 bg-zinc-100 shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset]">
+                Consider donating to help provide STEM opportunities to all
+                students at Woodson. By donating, you help foster student&apos;s
+                passions by enabling our team to purchase the necessary
+                materials, tools, and resources for building our robot. <Br />
+                Join us by empowering the next generation of innovators and
+                leaders.
+            </div>
+        </PinContainer>
     );
 }
