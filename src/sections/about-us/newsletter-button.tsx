d102a6fe352726a33fbafbb283604cc40557a314
Peter Bierma <zintensitydev@gmail.com>
2025-11-14 18:16:48 -0500

    Add a little effect to the newsletter button.

--- src/sections/about-us/newsletter-button.tsx
+++ src/sections/about-us/newsletter-button.tsx
@@ -8,7 +8,7 @@ export default function NewsletterButton() {
         <div>
             <button
                 onClick={() => modal.setOpen(true)}
-                className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] outline-none"
+                className="relative inline-flex h-12 overflow-hidden rounded-lg p-[1px] outline-none hover:-translate-y-1 transition-transform"
             >
                 <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#3845ff_0%,#ff4252_50%,#3845ff_100%)]"></span>
                 <div className="inline-flex space-x-1 h-full w-full cursor-pointer items-center justify-center rounded-lg bg-zinc-950 px-3 py-1 text-base md:text-lg font-extrabold text-white backdrop-blur-3xl">
