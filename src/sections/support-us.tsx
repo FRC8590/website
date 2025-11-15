396d9a3c2d7146143dd6121615c3676c4c552b8d
Peter Bierma <zintensitydev@gmail.com>
2025-11-15 11:43:49 -0500

    Add some pretty images.

--- src/sections/support-us.tsx
+++ src/sections/support-us.tsx
@@ -1,4 +1,5 @@
 import Br from "@/utils/br";
+import Image from "next/image";
 import { PinContainer } from "@/components/3d-pin";
 
 const DONATE_URL =
@@ -8,9 +9,9 @@ export default function SupportUs() {
     return (
         <div className="h-[40rem] w-full flex items-center justify-center ">
             <PinContainer title="Donate Now!" href={DONATE_URL}>
-                <div className="flex items-center justify-center w-full basis-full flex-col p-4 tracking-tight sm:basis-1/2 h-full dark:bg-grid-small-white/[0.05] bg-grid-small-black/[0.2] rounded-xl border border-[rgba(255,255,255,0.10)] dark:bg-zinc-950 hover:dark:bg-zinc-900 bg-zinc-100 shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset] hover:shadow-lg transition-all">
-                    <div className="text-xl !m-0 !p-0 font-light w-full">
-                        <div className="flex flex-row overflow-y-auto w-full">
+                <div className="flex justify-center items-center w-full basis-full flex-col tracking-tight sm:basis-1/2 h-full dark:bg-grid-small-white/[0.05] bg-grid-small-black/[0.2] rounded-xl border border-[rgba(255,255,255,0.10)] dark:bg-zinc-950 hover:dark:bg-zinc-900 bg-zinc-100 shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset] hover:shadow-lg transition-all">
+                    <div className="text-base md:text-xl !m-0 !p-0 font-light w-full">
+                        <div className="flex overflow-y-auto w-full">
                             <p className="p-8">
                                 Consider donating to help provide STEM
                                 opportunities to all students at Woodson. By
@@ -18,11 +19,37 @@ export default function SupportUs() {
                                 passions by enabling our team to purchase the
                                 necessary materials, tools, and resources for
                                 building our robot. <Br />
-                                <span className="font-medium text-3xl">
+                                <span className="font-medium text-lg md:text-3xl">
                                     Join us by empowering the next generation of
-                                    innovators and leaders.
+                                    innovators and leaders!
                                 </span>
                             </p>
+                        </div>
+                        <div className="grid grid-rows-1 grid-cols-3 auto-rows-fr gap-6 px-4">
+                            <div className="relative aspect-square">
+                                <Image
+                                    src="/pictures/why-donate-1.jpg"
+                                    fill
+                                    alt="Team Image"
+                                    className="rounded-xl grayscale hover:grayscale-0 object-cover"
+                                />
+                            </div>
+                            <div className="relative aspect-square">
+                                <Image
+                                    src="/pictures/why-donate-2.jpg"
+                                    fill
+                                    alt="Team Image"
+                                    className="rounded-xl grayscale hover:grayscale-0 object-cover"
+                                />
+                            </div>
+                            <div className="relative aspect-square">
+                                <Image
+                                    src="/pictures/why-donate-3.jpg"
+                                    fill
+                                    alt="Team Image"
+                                    className="rounded-xl grayscale hover:grayscale-0 object-cover"
+                                />
+                            </div>
                         </div>
                     </div>
                 </div>
