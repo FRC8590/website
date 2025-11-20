413ebb2f5d8ab33594c7cbe291e60ca4a0afcda5
Peter Bierma <zintensitydev@gmail.com>
2025-11-20 18:41:10 -0500

    Stupid key prop.

--- src/sections/sponsors/sponsor-list.tsx
+++ src/sections/sponsors/sponsor-list.tsx
@@ -4,54 +4,49 @@ import { motion } from "framer-motion";
 import { InfiniteMovingCards } from "@/components/infinite-moving-cards";
 
 const SPONSORS = [
-    <a href="https://woodsonhs.fcps.edu/" target="_blank">
+    <a href="https://woodsonhs.fcps.edu/" target="_blank" key={0}>
         <Image
             src="/sponsors/wt-ptso.png"
             width={100}
             height={100}
             alt="C.G. Woodson PTSO"
             className="sponsor-image"
-            key={0}
         />
     </a>,
-    <a href="https://www.ghaasfoundation.org/" target="_blank">
+    <a href="https://www.ghaasfoundation.org/" target="_blank" key={1}>
         <Image
             src="/sponsors/haas.png"
             width={100}
             height={100}
             alt="HAAS"
             className="sponsor-image"
-            key={1}
         />
     </a>,
-    <a href="https://www.synergybis.com/" target="_blank">
+    <a href="https://www.synergybis.com/" target="_blank" key={2}>
         <Image
             src="/sponsors/synergy.png"
             width={200}
             height={200}
             alt="Synergy"
             className="sponsor-image"
-            key={2}
         />
     </a>,
-    <a href="https://www.rtx.com/" target="_blank">
+    <a href="https://www.rtx.com/" target="_blank" key={3}>
         <Image
             src="/sponsors/rtx.png"
             width={200}
             height={200}
             alt="RTX"
             className="sponsor-image"
-            key={3}
         />
     </a>,
-    <a href="https://caringhandsvet.com/" target="_blank">
+    <a href="https://caringhandsvet.com/" target="_blank" key={4}>
         <Image
             src="/sponsors/caring-hands.png"
             width={100}
             height={100}
             alt="Caring Hands Animal Hospital"
             className="sponsor-image"
-            key={4}
         />
     </a>,
 ];
